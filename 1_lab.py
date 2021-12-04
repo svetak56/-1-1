e5ea203e29316491dc5f192cb893c4dc3e4e0e84
svetak56 <92648315+svetak56@users.noreply.github.com>
2021-12-05 00:28:51 +0300

    Update 1_lab.py

--- 1_lab.py
+++ 1_lab.py
@@ -1,12 +1,39 @@
-mixed_list = [3, .42345, 'eras', (3+5j), '173', 65535, -391.3237162346, None]
+str='т и м о ш и н а'
+key=str.split()
+#print(key)
+number=[20,10,14,16,26,10,15,1]
+value=[]
+for x in number:
+    value.append(x**3)
+#print(value)
+a={k:v for k, v in zip(key,value)}
 
-filtered_list = [x for x in mixed_list if x!=None]
+#Отсортировать по ключу в алфавитном порядке
+keysort_a={k: a[k] for k in sorted(a)} 
+#print('сортировка по алфавиту',keysort_a)
+with open('алфавитный порядок.txt', 'w') as file1: 
+  for key, value in keysort_a.items():
+    file1.write(f'{key}, {value}\n') #сохранение в файл
 
-tuple_list = (3, 1, .32, 'ryry', -3221.3321, None, None)
+#Отсортировать по значениям от меньшего к большему 
+valuesort_a={value: a[value] for value in sorted(a)}
+valuesort_a={value: key for key, value in valuesort_a.items()} 
+#print('сортировка от меньшего к большему',valuesort_a)
+with open('от меньшего к большему.txt', 'w') as file2:
+  for key, value in valuesort_a.items():
+    file2.write(f'{key}, {value}\n') #сохранение в файл
 
-dict_list = {'int': 16768, 'float': -1.3273, 'str': 'foo_cad', 'complex': (5+1.3j), 'ept': None}
-
-filt_dict = {key: value for key, value in dict_list.items() if len(key)>3 or value!=None}
-
-print(filt_dict)
-print(filtered_list)
+#Создать список из значений словаря и разделить его на два:
+#один из значений меньше среднего по списку, второй - среднее и выше.
+hige={} #для среднего и выше
+low={} #для ниже среднего
+sr_zn=sum(value for value in a.values())/8 #среднее знаение
+for key,value in a.items():
+    if value< sr_zn:
+        low[key] = value
+    else:
+        hige[key]=value
+print('больше среднего значения',hige)
+print('меньше среднего значения',low)
+    
+#print(a)
